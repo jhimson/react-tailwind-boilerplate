b4fab6bd147ae6043315b1106826aee51622163a
jhimson <jhimsonrayraboypamisa@gmail.com>
2020-12-16 23:29:17 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className='text-4xl text-blue-500'>Hello, world!!! WHATUP BITCHES</h1>
+      <h1 className='text-4xl text-blue-500'>Hello, world!!!</h1>
     </div>
   );
 }
